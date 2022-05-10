b07b5ddaeeb125fb3ba2b1204b1341647adfbe2c
YounesseELH <96134357+YounesseELH@users.noreply.github.com>
2022-05-10 22:24:37 +0100

    new

---
+++ Image classification web app/app.py
@@ -0,0 +1,35 @@
+from pyexpat import model
+from flask import Flask,render_template,request
+
+from keras.preprocessing.image import load_img 
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
+
+app = Flask(__name__)
+model = VGG16()
+
+@app.route('/',methods=['GET'])
+def mGet():
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+def prediction():
+    imageFile = request.files['image_file']
+    imagePath = "./images/"+imageFile.filename
+    imageFile.save(imagePath)
+
+    image = load_img(imagePath,target_size=(224,224))
+    image = img_to_array(image)
+    image = image.reshape((1,image.shape[0],image.shape[1],image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label = label[0][0]
+    classification = '%s (%.2f%%' %(label[1],label[2]*100)
+    return render_template('index.html',pred = classification)
+
+
+if __name__ == '__main__':
+    app.run(port=3000,debug=True)
